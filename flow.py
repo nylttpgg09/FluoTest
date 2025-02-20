6f45fea33f2c0bbf5aeca7fe23d005b9de305481
nylttpgg09 <nylttp1018@163.com>
2025-02-20 22:53:35 +0800

    1.美化一下界面，调整了欢迎界面和主页的顺序，调整界面大小，修复了测试的bug

--- flow.py
+++ flow.py
@@ -102,11 +102,15 @@ def calculate_area_ratio(peaks, peak_areas):
 class PlotCanvas(FigureCanvas):
     def __init__(self, parent=None):
         # 创建一个图形对象
-        self.fig, self.ax = plt.subplots(figsize=(5, 4))
+        self.fig, self.ax = plt.subplots(figsize=(4.7, 2.8))
+
         super().__init__(self.fig)
         self.setParent(parent)
 
     def plot(self, data, peaks):
+        # 清除上一次生成的图像
+        self.ax.clear()
+
         # 绘制原始曲线
         self.ax.plot(range(len(data)), data, label='Original Data')
 
@@ -155,6 +159,9 @@ class TextWindow(QMainWindow):
         self.button = QPushButton('返回', self)
         self.button.clicked.connect(self.on_main_click)
         self.layout.addWidget(self.button)
+
+        # 设置固定窗口大小为 470x280
+        self.setFixedSize(470, 280)
     def on_button_click(self):
         # 加载数据
         load_data()
@@ -200,15 +207,15 @@ class Welcome1Window(QMainWindow):
         # 如果进度条完成了100%，跳转到 main.ui
         if self.progress >= 100:
             self.timer.stop()  # 停止定时器
-            self.open_welcome_window()  # 打开主界面
+            self.open_main_window()  # 打开主界面
 
-    def open_welcome_window(self):
-        # 创建欢迎界面并显示
-        self.welcome_window = WelcomeWindow()
-        self.welcome_window.show()
 
-        # 关闭欢迎界面
-        self.close()
+    def open_main_window(self):
+        self.main_window = MainWindow()  # 创建主窗口对象
+        self.main_window.show()
+        self.close()  #
+
+
 
 class WelcomeWindow(QMainWindow):
     def __init__(self):
@@ -245,11 +252,19 @@ class MainWindow(QMainWindow):
         self.itemButton.clicked.connect(self.show_item_window)
         self.testButton.clicked.connect(self.show_test_window)
         self.close_button.clicked.connect(self.show_close_window)
+        self.setbox_button.clicked.connect(self.show_welcome_window)
 
     def show_fluo_window(self):
         self.fluo_window = FluoTestWindow()  # 创建 FluoTest 窗口对象
         self.fluo_window.show()
         self.close()  # 关闭当前
+
+    def show_welcome_window(self):
+        # 创建欢迎界面并显示
+        self.welcome_window = WelcomeWindow()
+        self.welcome_window.show()
+        # 关闭欢迎界面
+        self.close()
 
     def show_history_window(self):
         self.historys_window = HistorysWindow()
@@ -262,9 +277,9 @@ class MainWindow(QMainWindow):
         self.close()  # 关闭当前
 
     def show_test_window(self):
-        self.test_window = ChannelWindow()
+        self.test_window = TextWindow()  # 创建 FluoTest 窗口对象
         self.test_window.show()
-        self.close()  # 关闭当前
+        self.close()  #
     def show_close_window(self):
         self.close()  #
 
